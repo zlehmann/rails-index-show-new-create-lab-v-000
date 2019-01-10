80a0aa7c319e4d1c479af92f5aa45dd23e38df9f
Zachary Lehmann <zlehmann@gmail.com>
2019-01-10 19:37:25 +0000

    Automatically backed up by Learn

--- db/migrate/20190110193611_create_coupons.rb
+++ db/migrate/20190110193611_create_coupons.rb
@@ -2,7 +2,7 @@ class CreateCoupons < ActiveRecord::Migration[5.0]
   def change
     create_table :coupons do |t|
       t.string :coupon_code
-      t.string :store 
+      t.string :store
     end
   end
 end
